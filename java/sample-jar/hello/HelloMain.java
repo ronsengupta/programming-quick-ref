8955e1c65c422c997b542639fcf45bf083cfbc10
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2014-11-13 21:02:31 +0900

    Added jar sample

---
+++ java/sample-jar/hello/HelloMain.java
@@ -0,0 +1,11 @@
+package hello;
+
+import hello.lib.HelloLib;
+
+public class HelloMain {
+
+    public static void main(String[] args) {
+        HelloLib hellolib = new HelloLib();
+        hellolib.bar();
+    }
+}
---
+++ java/sample-jar/hello/lib/HelloLib.java
@@ -0,0 +1,7 @@
+package hello.lib;
+
+public class HelloLib {
+    public void bar() {
+        System.out.println("Hello Lib");
+    }
+}
