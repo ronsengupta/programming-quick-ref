e2afe33714a4d6b8f5f7fe47db6a16c211ab6e6f
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2015-05-01 17:33:28 +0900

    fix format

--- go/method/method.go
+++ go/method/method.go
@@ -3,28 +3,28 @@ package main
 import fmt "fmt"
 
 type Test struct {
-    Name string
+	Name string
 }
 
 func (t Test) hello() {
-    fmt.Printf("Hello, %s\n", t.Name)
+	fmt.Printf("Hello, %s\n", t.Name)
 }
 
 func (t *Test) goodby() {
-    fmt.Printf("Goodby, %s\n", t.Name)
+	fmt.Printf("Goodby, %s\n", t.Name)
 }
 
 func main() {
-    t := Test{Name: "nak3"}
+	t := Test{Name: "nak3"}
 
-    t.hello()   
-    t.goodby()
+	t.hello()
+	t.goodby()
 
-    f_hello := t.hello   
-    f_goodby := t.goodby
+	f_hello := t.hello
+	f_goodby := t.goodby
 
-    t.Name = "kenjiro" 
+	t.Name = "kenjiro"
 
-    f_hello()   
-    f_goodby()
+	f_hello()
+	f_goodby()
 }
